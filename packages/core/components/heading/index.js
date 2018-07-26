ca0ce236c06a104b8daf8a448fd26045f9ea85ad
Alex Kaminsky <akaminsky@azavea.com>
2018-07-26 14:06:00 -0400

    Add example common enum

--- packages/core/components/heading/index.js
+++ packages/core/components/heading/index.js
@@ -1,51 +1,68 @@
 /* @flow */
 
-import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import { Alignment } from "../../common/alignment";
+
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
 const Heading = styled.h1`
   color: black;
+  text-align: left;
 
-  ${({ blue }) => blue && css`
-    background: blue;
-  `}
+  ${props =>
+    props.blue &&
+    css`
+      background: blue;
+    `};
 
-  ${props => props.green && css`
-    background: green;
-  `}
-`
+  ${props =>
+    props.green &&
+    css`
+      background: green;
+    `};
+
+  ${props =>
+    props.alignment &&
+    css`
+      text-align: ${props.alignment};
+    `};
+`;
 
 Heading.propTypes = {
   blue: PropTypes.bool,
-  green: PropTypes.bool
-}
+  green: PropTypes.bool,
+  /**
+   * Alignment of the heading content
+   */
+  alignment: PropTypes.oneOf(Object.values(Alignment))
+};
 
-Heading.h1 = Heading.withComponent('h1').extend`
+Heading.h1 = Heading.withComponent("h1").extend`
   font-size: ;
   line-height: ;
 `;
 
-Heading.h2 = Heading.withComponent('h2').extend`
+Heading.h2 = Heading.withComponent("h2").extend`
   font-size: ;
   line-height: ;
 `;
 
-Heading.h3 = Heading.withComponent('h3').extend`
+Heading.h3 = Heading.withComponent("h3").extend`
   font-size: ;
   line-height: ;
 `;
 
-Heading.h4 = Heading.withComponent('h4').extend`
+Heading.h4 = Heading.withComponent("h4").extend`
   font-size: ;
   line-height: ;
 `;
 
-Heading.h5 = Heading.withComponent('h5').extend`
+Heading.h5 = Heading.withComponent("h5").extend`
   font-size: ;
   line-height: ;
 `;
 
-Heading.h6 = Heading.withComponent('h6').extend`
+Heading.h6 = Heading.withComponent("h6").extend`
   font-size: ;
   line-height: ;
 `;
--- packages/core/index.js
+++ packages/core/index.js
@@ -1,8 +1,10 @@
-export {default as Button} from './components/button';
-export {default as ButtonGroup} from './components/buttonGroup';
-export {default as PlainProgressBar} from './components/plainProgressBar';
-export {default as ProgressBar} from './components/progressBar';
-export {default as Tag} from './components/tag';
+/* All public components */
+export { default as Button } from "./components/button";
+export { default as ButtonGroup } from "./components/buttonGroup";
+export { default as PlainProgressBar } from "./components/plainProgressBar";
+export { default as ProgressBar } from "./components/progressBar";
+export { default as Tag } from "./components/tag";
 
-
-export { Colors, Intent } from './defaultTheme';
+/* Non-component public enums and methods */
+export { Colors, Intent } from "./defaultTheme";
+export * from "./common/alignment";
