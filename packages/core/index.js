f5fe4bd98d22859386c9b26cbb096093581bbb00
Alex Kaminsky <alexander.s.kaminsky@gmail.com>
2018-07-27 10:21:48 -0400

    Add example common enum (#14)

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,8 +1,12 @@
-export {default as Button} from './components/button';
-export {default as ButtonGroup} from './components/buttonGroup';
-export {default as PlainProgressBar} from './components/plainProgressBar';
-export {default as ProgressBar} from './components/progressBar';
-export {default as Tag} from './components/tag';
+/* All public components */
+export { default as Button } from "./components/button";
+export { default as ButtonGroup } from "./components/buttonGroup";
+export { default as PlainProgressBar } from "./components/plainProgressBar";
+export { default as ProgressBar } from "./components/progressBar";
+export { default as Tag } from "./components/tag";
 
+/* Theme related constants and methods */
+export * from "./defaultTheme";
 
-export { Colors, Intent } from './defaultTheme';
+/* Non-component common constants and methods */
+export * from "./common/alignment";
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,51 +1,51 @@
-const { createConfig, babel, postcss } = require('webpack-blocks');
-const path = require('path');
+const { createConfig, babel, postcss } = require("webpack-blocks");
+const path = require("path");
 
 module.exports = {
-    webpackConfig: createConfig([babel(), postcss()]),
-    sections: [
-        {
-          name: 'Introduction',
-          content: 'docs/introduction.md'
-        },
-        {
-            name: 'Colors',
-            content: 'docs/colors.md',
-            sections: [
-                {
-                    name: 'Usage Patterns',
-                    content: 'docs/colors/usage.md'
-                }
-            ]
-        },
-        {
-            name: 'Components',
-            content: 'docs/components.md',
-            sections: [
-                {
-                    name: 'Core',
-                    components: 'packages/core/components/**/index.js',
-                    usageMode: 'expand',
-                    exampleMode: 'expand'
-                },
-                {
-                    name: 'Chart',
-                    components: 'packages/chart/components/**/index.js',
-                    usageMode: 'expand',
-                    exampleMode: 'expand'
-                }
-            ]
-        }
-    ],
-    getExampleFilename(componentPath) {
-        return componentPath.replace('index.js', 'README.md');
+  webpackConfig: createConfig([babel(), postcss()]),
+  sections: [
+    {
+      name: "Introduction",
+      content: "docs/introduction.md"
     },
-    ignore: [
-        '**/*.spec.js',
-        '**/node_modules/**/*.js'
-    ],
-    pagePerSection: true,
-    styleguideComponents: {
-        Wrapper: path.join(__dirname, 'docs/DocsThemeWrapper')
+    {
+      name: "Colors",
+      content: "docs/colors.md",
+      sectionDepth: 2,
+      sections: [
+        {
+          name: "Usage Patterns",
+          content: "docs/colors/usage.md"
+        }
+      ]
+    },
+    {
+      name: "Components",
+      content: "docs/components.md",
+      pagePerSection: true,
+      sectionDepth: 2,
+      sections: [
+        {
+          name: "Core",
+          components: "packages/core/components/**/index.js",
+          usageMode: "expand",
+          exampleMode: "expand"
+        },
+        {
+          name: "Chart",
+          components: "packages/chart/components/**/index.js",
+          usageMode: "expand",
+          exampleMode: "expand"
+        }
+      ]
     }
+  ],
+  getExampleFilename(componentPath) {
+    return componentPath.replace("index.js", "README.md");
+  },
+  ignore: ["**/*.spec.js", "**/node_modules/**/*.js"],
+  pagePerSection: true,
+  styleguideComponents: {
+    Wrapper: path.join(__dirname, "docs/DocsThemeWrapper")
+  }
 };
