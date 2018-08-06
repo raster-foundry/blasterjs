5fd9f30934134f0aa77a70adb2872c47012ebf22
Alex Kaminsky <akaminsky@azavea.com>
2018-08-06 11:27:07 -0400

    Move to bash scripts for common tasks

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,18 +1,8 @@
 /* All public components */
-export { default as Button } from "./components/button";
-export { default as ButtonGroup } from "./components/buttonGroup";
-export { default as Heading } from "./components/heading";
-export { default as PlainProgressBar } from "./components/plainProgressBar";
-export { default as ProgressBar } from "./components/progressBar";
-export { default as Tag } from "./components/tag";
-export { default as Breadcrumbs } from "./components/breadcrumbs";
+export * from "./components";
 
 /* Theme related constants and methods */
 export * from "./defaultTheme";
 
 /* Non-component common constants and methods */
-export * from "./common/alignment";
-export * from "./common/direction";
-export * from "./common/position";
-export * from "./common/shape";
-export * from "./common/size";
+export * from "./common";
