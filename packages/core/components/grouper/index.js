172d034c30207f9c067cbb15212e843075713424
Scott Lederer <slederer@azavea.com>
2018-11-08 14:28:32 -0500

    WIP Grouper component

---
+++ packages/core/components/grouper/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import {} from "styled-system";
+
+const Grouper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: stretch;
+`;
+
+export default Grouper;
