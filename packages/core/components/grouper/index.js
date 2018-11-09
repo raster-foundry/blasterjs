13c0e6dcfcd029301ef37b98a9fc02b90a02234c
Scott Lederer <slederer@azavea.com>
2018-11-09 15:39:02 -0500

    Grouper component

--- packages/core/components/grouper/index.js
+++ packages/core/components/grouper/index.js
@@ -1,39 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { space } from "styled-system";
+import { space, themeGet } from "styled-system";
 
 const StyledGrouper = styled.div`
   ${space}
   display: flex;
   flex-flow: row nowrap;
   justify-content: flex-start;
-  align-items: stretch;
+  align-items: ${props => props.alignItems};
 
+  > * + * {
+    margin-left: ${props => props.gutter};
+  }
+`;
+
+const CompactGrouper = styled(StyledGrouper).attrs({
+  radius: props => themeGet(`radii.${props.borderRadius}`, props.borderRadius)
+})`
   > * {
-    border-radius: 0;
+    border-radius: 0 !important;
   }
 
   > :first-child {
-      border-radius: 10px 0 0 10px;
+    border-top-left-radius: ${props => props.radius} !important;
+    border-bottom-left-radius: ${props => props.radius} !important;
   }
 
   > :last-child {
-      border-radius: 0 10px 10px 0;
+    border-top-right-radius: ${props => props.radius} !important;
+    border-bottom-right-radius: ${props => props.radius} !important;
   }
 `;
 
 const Grouper = props => {
-  return (
-    <Grouper {...props}>
+  return props.gutter === 0 ? (
+    <CompactGrouper {...props}>
       {props.children}
-    </Grouper>
+    </CompactGrouper>
+  ) : (
+    <StyledGrouper {...props}>
+      {props.children}
+    </StyledGrouper>
   );
 };
 
 Grouper.propTypes = {
   ...space.PropTypes,
-  // borderRadius: PropTypes.oneOf(Object.values())
+  borderRadius: PropTypes.string,
+  gutter: PropTypes.string,
+  alignItems: PropTypes.string
+};
+
+Grouper.defaultProps = {
+  borderRadius: 0,
+  gutter: 0,
+  alignItems: "stretch"
 };
 
 export default Grouper;
