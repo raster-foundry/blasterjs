3f0f88634f36308eb5a391e6c58173f11fd2ebb3
Scott Lederer <slederer@azavea.com>
2018-11-12 13:58:08 -0500

    Add intent constant

--- packages/core/index.common.js
+++ packages/core/index.common.js
@@ -1,6 +1,8 @@
+export { default as README } from "./common/README";
 export { default as Alignment } from "./common/alignment";
 export { default as Density } from "./common/density";
 export { default as Direction } from "./common/direction";
+export { default as Intent } from "./common/intent";
 export { default as Position } from "./common/position";
 export { default as Shape } from "./common/shape";
 export { default as Size } from "./common/size";
