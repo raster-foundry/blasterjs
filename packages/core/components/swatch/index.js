30d6ca9aa68ac1b9041a4c0d7b0e0b3bb4a0628e
Scott Lederer <slederer@azavea.com>
2018-11-16 10:35:17 -0500

    Add shape prop to Swatch component

--- packages/core/components/swatch/index.js
+++ packages/core/components/swatch/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Badge from "../badge";
+import { Shape } from "../../common/shape";
 
 const StyledSwatch = styled(Badge)`
   width: 1em;
@@ -9,19 +10,40 @@ const StyledSwatch = styled(Badge)`
   padding: 0;
 `;
 
-const Swatch = ({color, children, ...otherProps}) => {
+const getBorderRadius = shape => {
+  switch (shape) {
+    case Shape.CIRCLE:
+      return 999;
+    case Shape.SQUARE:
+      return 0;
+    case Shape.ROUNDED:
+    default:
+      return "small";
+  }
+};
+
+const Swatch = ({color, shape, children, ...otherProps}) => {
+  const radius = getBorderRadius(shape);
+
   return (
-    <StyledSwatch {...otherProps} bg={color} title={color} />
+    <StyledSwatch
+      {...otherProps}
+      bg={color}
+      title={color}
+      borderRadius={radius}
+    />
   );
 };
 
 Swatch.propTypes = {
   ...Badge.propTypes,
-  color: PropTypes.string.isRequired
+  color: PropTypes.string.isRequired,
+  shape: PropTypes.oneOf(Object.values(Shape))
 };
 
 Swatch.defaultProps = {
-  ...Badge.defaultProps
+  ...Badge.defaultProps,
+  shape: Shape.ROUNDED
 };
 
 export default Swatch;
