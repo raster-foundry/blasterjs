f66f136ce3de4d3685909d8609daa697a19d662a
Scott Lederer <slederer@azavea.com>
2018-11-28 14:55:59 -0500

    Add Box component

--- packages/core/index.components.js
+++ packages/core/index.components.js
@@ -1,4 +1,5 @@
 export { default as Badge } from "./components/badge";
+export { default as Box } from "./components/box";
 export { default as Brand } from "./components/brand";
 export { default as Breadcrumbs } from "./components/breadcrumbs";
 export { default as Button } from "./components/button";
