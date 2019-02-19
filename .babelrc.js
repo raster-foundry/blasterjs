dec1fd3561bb79273974dbdec73c8ec90aa85957
Alex Kaminsky <akaminsky@azavea.com>
2019-02-19 13:08:37 -0500

    Remove flow

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'presets': ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+  'presets': ['@babel/preset-env', '@babel/preset-react'],
   'env': {'test': {'plugins': ['@babel/plugin-transform-modules-commonjs']}},
   'plugins': [[
     'babel-plugin-styled-components',
