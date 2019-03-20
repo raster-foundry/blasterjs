670377d1851004797804d75eca87c6f261b51cb6
Alex Kaminsky <akaminsky@azavea.com>
2019-03-20 16:55:58 -0400

    Add storysource addon

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre'
+  });
+
+  // Return the altered config
+  return config;
+};
