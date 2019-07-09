a009197b76aeeac7568802db8211bd3412579e20
Scott Lederer <slederer@azavea.com>
2019-07-09 12:41:30 -0400

    Update Heading to new theme system

--- packages/core/components/heading/index.js
+++ packages/core/components/heading/index.js
@@ -1,43 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { themeGet } from "styled-system";
-import { COMMON, TYPOGRAPHY, MISC, LAYOUT } from "../../constants";
+import { COMMON, LAYOUT, POSITION, TYPOGRAPHY, FLEX_ITEM } from "../../constants";
 
 const Heading = styled.h1`
-  color: ${themeGet("colors.heading.color")};
-  font-family: ${themeGet("fonts.heading.fontFamily")};
-  font-weight: ${themeGet("fontWeights.heading.fontWeight")};
-  line-height: ${themeGet("lineHeights.heading.lineHeight")};
-  margin-top: ${themeGet("space.heading.m")};
+  color: ${themeGet("heading.colors.color")};
+  font-family: ${themeGet("heading.fonts.fontFamily")};
+  font-weight: ${themeGet("heading.fontWeights.fontWeight")};
+  line-height: ${themeGet("heading.lineHeights.lineHeight")};
+  margin-top: ${themeGet("heading.space.mt")};
+  font-size: ${props => themeGet(`heading.fontSizes.${props.as}`)};
 
-  ${props => {
-    const size = {
-      h1: 7,
-      h2: 6,
-      h3: 5,
-      h4: 4,
-      h5: 3,
-      h6: 2
-    }[props.as];
-
-    return css`
-      font-size: ${props => themeGet(`fontSizes.${size}`)};
-    `;
-  }}
-
-  ${themeGet("styles.heading")};
+  ${themeGet("heading.styles")}
   ${COMMON}
   ${LAYOUT}
-  ${MISC}
+  ${POSITION}
   ${TYPOGRAPHY}
+  ${FLEX_ITEM}
 `;
 
 Heading.propTypes = {
   ...COMMON.propTypes,
   ...LAYOUT.propTypes,
-  ...MISC.propTypes,
+  ...POSITION.propTypes,
   ...TYPOGRAPHY.propTypes,
+  ...FLEX_ITEM.propTypes,
   as: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6"])
 };
 
--- packages/core/theme/components/heading/index.js
+++ packages/core/theme/components/heading/index.js
@@ -2,13 +2,21 @@ import { css } from "styled-components";
 
 export const theme = {
   space: {
-    m: 0
+    mt: 0
   },
   colors: {
     color: "textBase"
   },
   fonts: {
     fontFamily: "display"
+  },
+  fontSizes: {
+    h1: 7,
+    h2: 6,
+    h3: 5,
+    h4: 4,
+    h5: 3,
+    h6: 2
   },
   fontWeights: {
     fontWeight: "700"
