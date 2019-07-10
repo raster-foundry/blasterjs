86347a60338f75284918a494edb05f2eda91432c
Scott Lederer <slederer@azavea.com>
2019-07-10 12:26:22 -0400

    Update Table to new theme system
    - Replace `density` prop with `compact` boolean prop
    
    Closes #157

--- packages/core/components/table/index.js
+++ packages/core/components/table/index.js
@@ -3,14 +3,9 @@ import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 import { borders, themeGet } from "styled-system";
 import { rgba } from "polished";
-import Box from "../box";
-import { Density } from "../../common/density";
+import { COMMON, BORDER, LAYOUT, TYPOGRAPHY } from "../../constants";
 
-const Table = styled(Box).attrs({
-  tag: 'table'
-})`
-  ${borders}
-
+const Table = styled.table`
   th {
     font-weight: bold;
   }
@@ -19,68 +14,97 @@ const Table = styled(Box).attrs({
   th,
   td {
     border-style: solid;
-    border-color: ${props => themeGet(`colors.${props.borderColor}`, props.borderColor)};
+    ${props => props.borderColor
+      ? css`border-color: ${themeGet(`colors.${props.borderColor}`, props.borderColor)};`
+      : css`border-color: ${themeGet('table.colors.borderColor')};`
+    }
   }
 
   ${props => props.borderHeader && css`
     th {
       border-bottom-style: solid;
-      border-bottom-color: ${props => themeGet(`colors.${props.borderColor}`, props.borderColor)};
-      border-bottom-width: ${props => `${props.borderWidth}px`};
+      ${props => props.borderColor
+        ? css`border-color: ${themeGet(`colors.${props.borderColor}`, props.borderColor)};`
+        : css`border-color: ${themeGet('table.colors.borderColor')};`
+      }
+      ${props => props.borderWidth
+        ? css`border-bottom-width: ${props.borderWidth}px};`
+        : css`border-bottom-width: ${themeGet('table.borderWidths.borderWidth')}px;`
+      }
     }
   `}
 
   ${props => (props.bordered || props.borderOutside) && css`
     & {
-      border-width: ${props => `${props.borderWidth}px`};
+      ${props => props.borderWidth
+        ? css`border-width: ${props.borderWidth}px};`
+        : css`border-width: ${themeGet('table.borderWidths.borderWidth')}px;`
+      }
     }
   `}
 
   ${props => (props.bordered || props.borderInside || props.borderRows) && css`
     tr:not(:last-child) td {
-      border-bottom-width: ${props => `${props.borderWidth}px`};
+      ${props => props.borderWidth
+        ? css`border-bottom-width: ${props.borderWidth}px};`
+        : css`border-bottom-width: ${themeGet('table.borderWidths.borderWidth')}px;`
+      }
+
     }
   `}
 
   ${props => (props.bordered || props.borderInside || props.borderColumns) && css`
     th:not(:first-child),
     td:not(:first-child) {
-      border-left-width: ${props => `${props.borderWidth}px`};
+      ${props => props.borderWidth
+        ? css`border-left-width: ${props.borderWidth}px};`
+        : css`border-left-width: ${themeGet('table.borderWidths.borderWidth')}px;`
+      }
     }
   `}
 
   ${props => props.striped && css`
     tr:nth-child(odd) td {
-      background-color: ${props => rgba(themeGet(`colors.${props.stripeColor}`, props.stripeColor)(props), 0.15)};
+      ${props => props.stripeColor
+        ? css`background-color: ${rgba(themeGet(`colors.${props.stripeColor}`, props.stripeColor)(props), 0.15)};`
+        : css`background-color: ${rgba(themeGet('table.colors.stripeColor')(props), 0.15)};`
+      }
     }
   `}
 
   ${props => props.hoverable && css`
     tr:hover td,
     tr:active td {
-      background-color: ${props => rgba(themeGet(`colors.${props.hoverColor}`, props.hoverColor)(props), 0.15)};
+      ${props => props.hoverColor
+        ? css`background-color: ${rgba(themeGet(`colors.${props.hoverColor}`, props.hoverColor)(props), 0.15)};`
+        : css`background-color: ${rgba(themeGet('table.colors.hoverColor')(props), 0.15)};`
+      }
       cursor: ${props => props.hoverCursor};
     }
   `}
 
-  ${props => props.density === Density.COMFORTABLE && css`
-    th,
-    td {
-      padding: ${themeGet('space.2')};
-    }
-  `}
+  ${props => {
+    const density = props.compact ? 'compact' : 'comfortable';
+    return css`
+      th,
+      td {
+        padding: ${themeGet(`table.space.${density}`)};
+      }
+    `
+  }}
 
-  ${props => props.density === Density.COMPACT && css`
-    th,
-    td {
-      padding: ${themeGet('space.1')};
-    }
-  `}
+  ${themeGet("table.styles")}
+  ${COMMON}
+  ${BORDER}
+  ${LAYOUT}
+  ${TYPOGRAPHY}
 `;
 
 Table.propTypes = {
-  ...Box.propTypes,
-  ...borders.propTypes,
+  ...COMMON.propTypes,
+  ...BORDER.propTypes,
+  ...LAYOUT.propTypes,
+  ...TYPOGRAPHY.propTypes,
   borderHeader: PropTypes.bool,
   bordered: PropTypes.bool,
   borderInside: PropTypes.bool,
@@ -94,7 +118,7 @@ Table.propTypes = {
   hoverable: PropTypes.bool,
   hoverColor: PropTypes.string,
   hoverCursor: PropTypes.string,
-  density: PropTypes.oneOf(Object.values(Density)).isRequired
+  compact: PropTypes.bool
 };
 
 Table.defaultProps = {
@@ -104,14 +128,10 @@ Table.defaultProps = {
   borderRows: false,
   borderColumns: false,
   borderOutside: false,
-  borderColor: "table.borderColor",
-  borderWidth: 1,
   striped: false,
-  stripeColor: "table.stripeColor",
   hoverable: false,
-  hoverColor: "table.hoverColor",
   hoverCursor: "default",
-  density: Density.COMFORTABLE,
+  compact: false
 };
 
 export default Table;
