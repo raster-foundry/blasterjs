390e921b1e7f92e6e8cd79966bc98b9002d731cb
Scott Lederer <slederer@azavea.com>
2019-07-10 15:36:24 -0400

    Update Brand to new theme system
    Closed #168

--- packages/core/components/brand/index.js
+++ packages/core/components/brand/index.js
@@ -1,17 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { width, themeGet } from "styled-system";
+import { themeGet } from "styled-system";
 import Image from "../image";
-import A from "../a";
 import { extractComponentFromChildren } from "../../utils";
+import { COMMON, LAYOUT, TYPOGRAPHY, FLEX_ITEM } from "../../constants";
 
-const StyledBrand = styled(A)`
-  color: ${props => (!props.color ? themeGet("brand.colors.color") : "")};
+const StyledBrand = styled.a`
   display: inline-block;
   text-decoration: none;
-  font-weight: 600;
   vertical-align: middle;
+
+  color: ${themeGet("brand.colors.color")};
+  font-family: ${themeGet("brand.fonts.font")};
+  font-size: ${themeGet("brand.fontSizes.fontSize")};
+  font-weight: ${themeGet("brand.fontWeights.fontWeight")};
+
+  &:focus {
+    color: ${props =>
+      themeGet(`colors.${props.colorFocus}`, "brand.colors.colorFocus")};
+  }
+
+  &:hover {
+    color: ${props =>
+      themeGet(`colors.${props.colorHover}`, "brand.colors.colorHover")};
+  }
+
+  &:active {
+    color: ${props =>
+      themeGet(`colors.${props.colorActive}`, "brand.colors.colorActive")};
+  }
+
+  ${themeGet("brand.styles")}
+  ${COMMON}
+  ${LAYOUT}
+  ${TYPOGRAPHY}
+  ${FLEX_ITEM}
 `;
 
 const Brand = ({
@@ -19,32 +43,35 @@ const Brand = ({
   title,
   imagesrc,
   width,
+  height,
   children: _children,
   ...props
 }) => {
-  const [children, tooltip] = extractComponentFromChildren(
-    _children,
-    "Tooltip"
-  );
+
+  const [children, tooltip] = extractComponentFromChildren(_children, "Tooltip");
 
   return (
-    <StyledBrand href={href} {...props}>
-      {imagesrc ? (
-        <Image src={imagesrc} alt={title} width={width} />
-      ) : (
-        <span>{title}</span>
-      )}
+    <StyledBrand href={href} width={width} height={height} {...props}>
+      {imagesrc
+        ? <Image src={imagesrc} alt={title} width={width} height={height} />
+        : title
+      }
       {tooltip}
     </StyledBrand>
   );
 };
 
 Brand.propTypes = {
-  ...A.propTypes,
-  ...width.propTypes,
+  ...COMMON.propTypes,
+  ...LAYOUT.propTypes,
+  ...TYPOGRAPHY.propTypes,
+  ...FLEX_ITEM.propTypes,
   imagesrc: PropTypes.string,
   href: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  colorFocus: PropTypes.string,
+  colorHover: PropTypes.string,
+  colorActive: PropTypes.string
 };
 
 export default Brand;
--- packages/core/theme/components/brand/index.js
+++ packages/core/theme/components/brand/index.js
@@ -1,5 +1,20 @@
+import { css } from "styled-components";
+
 export const theme = {
   colors: {
-    color: "link"
-  }
+    color: "link",
+    colorFocus: "link",
+    colorHover: "linkHover",
+    colorActive: "linkHover"
+  },
+  fonts: {
+    font: "display"
+  },
+  fontSizes: {
+    fontSize: 4
+  },
+  fontWeights: {
+    fontWeight: 600
+  },
+  styles: css``
 };
