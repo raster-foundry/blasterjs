43e0128104a5ab3b6bcbdbecb4c993be8d161f27
Matt Williams <mwilliams@azavea.com>
2019-07-11 11:11:49 -0400

    Fix dependency issues  (#200)
    * initial discovery trial and error for build issues
    
    * updated each package json to fix a variety of conflicts
    
    * move npm-watch to devDependency

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,15 @@ module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
   env: { test: { plugins: ["@babel/plugin-transform-modules-commonjs"] } },
   plugins: [
-    ["babel-plugin-styled-components", { displayName: true, fileName: false }]
+    [
+      "babel-plugin-styled-components",
+      {
+        displayName: true,
+        ssr: true,
+        fileName: false,
+        minify: true,
+        transpileTemplateLiterals: false
+      }
+    ]
   ]
 };
