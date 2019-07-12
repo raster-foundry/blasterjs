a34e179df9e052a13dc32b67ce1bc420aded2e96
Scott Lederer <slederer@azavea.com>
2019-07-12 14:21:22 -0400

    Update Textarea to new theme system
    Closes #166

---
+++ packages/core/theme/components/textarea/index.js
@@ -0,0 +1,35 @@
+import { css } from "styled-components";
+
+export const theme = {
+  space: {
+    p: 1
+  },
+  colors: {
+    bg: "white",
+    color: "textBase",
+    borderColor: "textBase",
+    borderColorFocus: "primary",
+    borderColorInvalid: "red",
+    borderColorDisabled: "gray400",
+    bgFocus: "inherit",
+    bgInvalid: "inherit",
+    bgDisabled: "gray300",
+    shadowColorFocus: "primary"
+  },
+  fonts: {
+    fontFamily: "inherit"
+  },
+  fontSizes: {
+    fontSize: 2
+  },
+  lineHeights: {
+    lineHeight: "base"
+  },
+  borders: {
+    border: "1px solid"
+  },
+  radii: {
+    borderRadius: 0
+  },
+  styles: css``
+};
