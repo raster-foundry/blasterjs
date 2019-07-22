541b2c65b15da6a2fa3c27d87b4baa399314cf94
Scott Lederer <lederer@users.noreply.github.com>
2019-07-22 14:59:57 -0400

    More theme updates (#203)
    * Set body font properties from theme
    
    * Remove import cruft
    
    * Update Table to new theme system
    
    - Replace `density` prop with `compact` boolean prop
    
    Closes #157
    
    * Simplify Grouper and update to new theme system
    
    Applies the gutter prop (or theme value) as margin to every side of every
    child, to ensure rows have a gutter when wrapping. A negative margin equal
    to the children's margin is given to an intermediate wrapping div to
    ensure the gutter doesn't affect extrinsic layout.
    
    - Remove `density` prop
    - Remove `compact` prop
    - Remove `direction` prop, replace with `vertical` boolean prop
    - Remove `borderRadius` prop and all auto-rounded corners
    - Add `align` prop, passed to `align-items`
    
    Closes #154
    
    * Update Brand to new theme system
    
    Closed #168
    
    * Update Breadcrumbs to new theme system
    
    Closes #169

--- packages/core/theme/components/table/index.js
+++ packages/core/theme/components/table/index.js
@@ -1,7 +1,17 @@
+import { css } from "styled-components";
+
 export const theme = {
+  space: {
+    comfortable: 2,
+    compact: 1
+  },
   colors: {
     borderColor: "gray200",
     stripeColor: "gray200",
     hoverColor: "primary"
-  }
+  },
+  borderWidths: {
+    borderWidth: 1
+  },
+  styles: css``
 };
